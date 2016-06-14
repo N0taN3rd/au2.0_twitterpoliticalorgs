2bc219da36e52091f82d06115262e89a3de1759f
John Berlin <n0tan3rd@gmail.com>
2016-06-14 17:17:17 -0400

    my changes

--- parseFecZips.py
+++ parseFecZips.py
@@ -3,17 +3,18 @@ import os
 from os import walk
 from os import path
 
-dir = 'datafiles'
-outputDir = path.join(dir, 'output')
-os.mkdir(outputDir)
+if __name__ == '__main__':
+    dir = 'datafiles'
+    outputDir = path.join(dir, 'output')
+    os.mkdir(outputDir)
 
-for folder in os.listdir(dir):
-  if os.path.isdir(folder):
-    outFolder = path.join(outputDir, folder)
-    os.mkdir(outFolder)
-    for file in os.listdir(folder):
-        textRTF = open(file), "a")
-        textPlain = open(path.join(outFolder, file),'w+')
-        for oldLine in textRTF:
-          newLine = oldLine.rstrip().split('')
-          textPlain.write(" ".join(newLine))  
+    for folder in os.listdir(dir):
+      if os.path.isdir(folder):
+        outFolder = path.join(outputDir, folder)
+        os.mkdir(outFolder)
+        for file in os.listdir(folder):
+            textRTF = open(file, "a")
+            textPlain = open(path.join(outFolder, file),'w+')
+            for oldLine in textRTF:
+              newLine = oldLine.rstrip().split('')
+              textPlain.write(" ".join(newLine))
