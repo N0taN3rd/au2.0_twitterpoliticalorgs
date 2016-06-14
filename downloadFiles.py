726c012281e282a8bc86e4e052a8db33b629ceb2
John Berlin <n0tan3rd@gmail.com>
2016-06-14 14:25:21 -0400

    download script written

---
+++ downloadFiles.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import re
+import zipfile
+from urllib import urlopen
+from io import BytesIO
+
+if __name__ == '__main__':
+    with open('Index of _FEC_electronic_.html','r') as r:
+
+        dlSoup = BeautifulSoup(r,"lxml")
+    for td in dlSoup.find_all('a',href=True):
+
+        match = re.search('^[a-z.:/A-Z]+((\d{4})\d+)\.zip$',td['href'])
+        link = td['href']
+        if match:
+            year = int(match.group(2))
+            if year >= 2015:
+                print match.group(1)
+                zipresp = urlopen(link)
+                zipf = zipfile.ZipFile(BytesIO(zipresp.read()))
+                zipf.extractall('datafiles/{0}'.format(match.group(1)))
+                zipf.close()
+                zipresp.close()
+
+
