4b3e0bd0724f5a805f57a54f2494a9324a7e78dd
John Berlin <n0tan3rd@gmail.com>
2016-06-14 16:57:42 -0400

    tag_extraction file

--- tag_extraction.py
+++ tag_extraction.py
@@ -1,6 +1,7 @@
 from nltk.tag import StanfordNERTagger
-from nltk.tokenize import TweetTokenizer
+from nltk.tokenize import TweetTokenizer,WordPunctTokenizer
 import csv
+import os
 
 # download = True
 # if download:
@@ -11,11 +12,42 @@ import csv
 if __name__ == '__main__':
     print "hi"
     tweetToke = TweetTokenizer()
+    wordToke = WordPunctTokenizer()
     o = open('datafiles/clinton.csv')
-    st = StanfordNERTagger('english.all.3class.distsim.crf.ser.gz')
+
+    st = StanfordNERTagger(os.path.join(os.getcwd(),'stanfordModel/classifiers/english.all.3class.distsim.crf.ser.gz'),
+                           os.path.join(os.getcwd(), 'stanfordModel/stanford-ner-3.6.0.jar'))
+
+    # 'create_at', 'twitter_id', 'hashtags', 'twitter_url','person','organization'
+    taggedFile = open('taggedClinton.csv','w+')
+    headers = ['create_at', 'twitter_id', 'hashtags', 'twitter_url','person','organization']
+    tweetWriter = csv.DictWriter(taggedFile,headers)
+    tweetWriter.writeheader()
     for row in csv.DictReader(o):
         #
         print row['text']
-        print st.tag(tweetToke.tokenize(row['text']))
+        csvrow = {'create_at': row['created_at'],
+                  'twitter_id': row['twitter_id'],
+                  'hashtags': row['hashtags'],
+                  'twitter_url': row['twitter_url']
+                  }
+        for tagged in st.tag(wordToke.tokenize(row['text'])):
+            org = []
+            person = []
+
+            wasOrg = False
+            wasPerson = False
+            if tagged[1] == 'ORGANIZATION':
+                csvrow['organization'] = tagged[0]
+                wasOrg = True
+                print wasOrg
+
+            if tagged[1] == 'PERSON':
+                csvrow['person'] = tagged[0]
+                wasPerson = True
+                print wasPerson
+
+            if wasOrg or wasPerson:
+                tweetWriter.writerow(csvrow)
         print '\n'
     # tagger = StanfordNERTagger('datafile/clinton.csv')
